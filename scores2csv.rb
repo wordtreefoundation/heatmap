1a4fd997d09d1e5f068c2f35e99e662628429626
Duane Johnson <duane.johnson@gmail.com>
2015-06-23 13:01:36 -0600

    Add matching biblical phrases

--- scores2csv.rb
+++ scores2csv.rb
@@ -6,17 +6,28 @@ end
 
 tx, ty = nil, nil
 i = 1
+phrases = {}
 $stdin.each_line do |line|
-  score, titleX, titleY, x, preambleX, y, preambleY = line.chomp.split("\t")
+  score, titleX, titleY, x, preambleX, y, preambleY,
+  p1score, p1text, p2score, p2text, p3score, p3text = line.chomp.split("\t")
 
-  if tx != titleX || ty != titleY
-    tx, ty = titleX, titleY
-    puts %{</pre>}
-    puts %{<pre data-name="#{titleX}-#{titleY}" id="csv-#{i}">}
-    i += 1
-  end
+  # if tx != titleX || ty != titleY
+  #   tx, ty = titleX, titleY
+  #   puts %{</pre>}
+  #   puts %{<pre data-name="#{titleX}-#{titleY}" id="csv-#{i}">}
+  #   i += 1
+  # end
 
-  show (x.to_i-1), (y.to_i-1), -(score.to_f.round(3))
+
+  show x.to_i, y.to_i, -(score.to_f.round(3))
+  
+  phrases[y] ||= {}
+  phrases[y][x] = [
+    [-p1score.to_f.round(3), p1text],
+    [-p2score.to_f.round(3), p2text],
+    [-p3score.to_f.round(3), p3text]
+  ]
+  
   # {
   #   score: score,
   #   titleX: titleX,
@@ -26,4 +37,9 @@ $stdin.each_line do |line|
   # }
 end
 
-# puts JSON::pretty_generate(data)
+phrases.each_pair do |y, xs|
+  File.open("data-#{y}.json", "w") do |file|
+    file.puts("callback(" + xs.to_json + ")")
+  end
+end
+# puts JSON::pretty_generate(phrases)
